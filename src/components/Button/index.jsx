edb578d2992e0217013663763f228c4ffb7d7663
ALberto <albertolutzdias@gmail.com>
2020-08-28 23:49:58 -0300

    Criando a interface da plataforma de vídeos baseada na Netflix

---
+++ src/components/Button/index.jsx
@@ -0,0 +1,24 @@
+import styled from 'styled-components';
+
+const Button =  styled.button `
+  color: var(--white);
+  border: 1px solid var(--backEnd);
+  box-sizing: border-box;
+  cursor: pointer;
+  padding: 16px 24px;
+  font-style: normal;
+  font-weight: bold;
+  font-size: 16px;
+  outline: none;
+  border-radius: 5px;
+  text-decoration: none;
+  display: inline-block;
+  transition:  .3s;
+  &:hover,
+  &:focus {
+   background-color: var(--purple);
+}
+
+`;
+
+export default Button;
